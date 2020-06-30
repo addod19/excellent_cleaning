123661f2e69016c6033dcdcab3afb96812b11d30
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-30 10:34:08 +0000

    add hero, banner and import banner and hero to home

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Hero.css';
+
+const Hero = ( {children, hero} ) => {
+    return(
+        <header className={hero}>
+            { children }
+        </header>
+    )
+}
+
+Hero.defaultProps = {
+    hero: 'defaultHero',
+};
+
+export default Hero;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Hero from '../../components/Hero';
+import Banner from '../../components/Banner';
 
 const Home = () => {
     return(
         <>
-            <h1>Home page</h1>
+            <Hero>
+                <Banner title='Excellent Cleaning' subtitle="As low as $70">
+                    <Link to='about' className='btn-primary'>
+                        Excellent Cleaning
+                    </Link>
+                </Banner>
+            </Hero>
+            
         </>
     )
 }
